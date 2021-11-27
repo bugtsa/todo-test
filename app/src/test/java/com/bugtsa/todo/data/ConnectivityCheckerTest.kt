86535b2abc80644982873e5d1a13eac38ff8132a
Vladimir Polkhovtsev <bugtsa@gmail.com>
2021-11-27 16:14:20 +0300

    - mockito удален из тестов - подняты версии библиотек и плагинов

--- app/src/test/java/com/bugtsa/todo/data/ConnectivityCheckerTest.kt
+++ app/src/test/java/com/bugtsa/todo/data/ConnectivityCheckerTest.kt
@@ -3,23 +3,23 @@ package com.bugtsa.todo.data
 import android.content.Context
 import com.bugtsa.todo.network.utils.connectivity.ConnectivityChecker
 import com.bugtsa.todo.network.utils.connectivity.ConnectivityCheckerImpl
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
 import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Test
 import org.koin.test.AutoCloseKoinTest
 
 class ConnectivityCheckerTest : AutoCloseKoinTest() {
 
-    private val context = mock<Context>()
+    private val context: Context = mockk()
 
     private lateinit var connectivityChecker: ConnectivityChecker
 
     @Before
     fun setup() {
         clearAllMocks()
-        whenever(context.applicationContext).thenReturn(context)
+        every {context.applicationContext } returns context
         connectivityChecker = ConnectivityCheckerImpl(context)
     }
 
